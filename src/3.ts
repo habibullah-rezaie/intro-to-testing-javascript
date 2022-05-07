1820fd81a61f91e95dbd48c94bde9cae61ac7c89
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-08 01:49:52 +0430

    Create my own testing framework

--- src/3.ts
+++ src/3.ts
@@ -1,15 +1,19 @@
 import * as assert from "assert";
-import { sum } from "./math";
+import { sub, sum } from "./math";
 
 let result, expected;
+
 result = sum(7, 3);
 expected = 10;
+expect(result).toBe(expected);
 
+result = sub(7, 3);
+expected = 4;
 expect(result).toBe(expected);
 
 function expect(actual: any) {
   function toBe(expected) {
-    return assert.strictEqual(actual, expected);
+    if (actual !== expected) throw new Error(`${actual} is not equal to ${expected}.`)
   }
   return {
     toBe,
